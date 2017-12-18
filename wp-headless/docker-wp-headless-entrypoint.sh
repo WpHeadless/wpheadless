8e5ad2ea03a848781ccea252e710f851cca4a6bc
Aleksandr Tsertkov <tsertkov@gmail.com>
2017-12-18 03:03:29 +0100

    Add wp-headless docker-compose project

---
+++ wp-headless/docker-wp-headless-entrypoint.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/env bash
+
+set -e
+set -o pipefail
+
+test -e index.php -a -e wp-includes/version.php || exit 0
+wp core is-installed --allow-root && exit
+
+# enable www-data user
+usermod -s /bin/bash www-data
+
+su www-data <<'EOF'
+wp core multisite-install \
+  --url="$WORDPRESS_URL" \
+  --title="$WORDPRESS_TITLE" \
+  --admin_email="$WORDPRESS_ADMIN_EMAIL" \
+  --skip-email
+
+wp plugin activate custom-post-type-ui --network
+wp plugin activate advanced-custom-fields --network
+wp plugin activate acf-to-rest-api --network
+wp plugin activate amazon-web-services --network
+wp plugin activate wordpress-importer --network
+wp plugin activate admin-menu-editor --network
+
+wp plugin activate amazon-s3-and-cloudfront --network
+sed -i "2idefine('DBI_AWS_ACCESS_KEY_ID', '${DBI_AWS_ACCESS_KEY_ID}');" wp-config.php
+sed -i "2idefine('DBI_AWS_SECRET_ACCESS_KEY', '${DBI_AWS_SECRET_ACCESS_KEY}');" wp-config.php
+EOF
+
+# disable www-data user
+usermod -s /bin/sbin/nologin www-data
