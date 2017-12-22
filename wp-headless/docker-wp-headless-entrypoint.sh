cad721a6cdafcbfc876785ceb590f99bc187dcdb
Aleksandr Tsertkov <tsertkov@gmail.com>
2017-12-22 14:18:21 +0100

    Hardcode config vars into docker-compose files

--- wp-headless/docker-wp-headless-entrypoint.sh
+++ wp-headless/docker-wp-headless-entrypoint.sh
@@ -4,34 +4,44 @@ set -e
 set -o pipefail
 
 test -e index.php -a -e wp-includes/version.php || exit 0
-wp core is-installed --allow-root && exit
 
 # enable www-data user
 usermod -s /bin/bash www-data
 
 su www-data <<'EOF'
-wp core multisite-install \
-  --url="$WORDPRESS_URL" \
-  --title="$WORDPRESS_TITLE" \
-  --admin_email="$WORDPRESS_ADMIN_EMAIL" \
-  --skip-email
+set -eo pipefail
 
-sed -i "2idefine('DBI_AWS_ACCESS_KEY_ID', '${DBI_AWS_ACCESS_KEY_ID}');" wp-config.php
-sed -i "2idefine('DBI_AWS_SECRET_ACCESS_KEY', '${DBI_AWS_SECRET_ACCESS_KEY}');" wp-config.php
-sed -i "2idefine('FORCE_SSL_ADMIN', ${FORCE_SSL_ADMIN});" wp-config.php
-sed -i "2idefine('JWT_AUTH_SECRET_KEY', '${JWT_AUTH_SECRET_KEY}');" wp-config.php
-sed -i "2idefine('WP_DEFAULT_THEME', '${WP_DEFAULT_THEME}');" wp-config.php
-
-wp plugin activate custom-post-type-ui --network
-wp plugin activate advanced-custom-fields --network
-wp plugin activate acf-to-rest-api --network
-wp plugin activate amazon-web-services --network
-wp plugin activate wordpress-importer --network
-wp plugin activate amazon-s3-and-cloudfront --network
-wp plugin activate jwt-authentication-for-wp-rest-api --network
+if ! wp core is-installed --network; then
+  wp core multisite-install \
+    --url="$WORDPRESS_URL" \
+    --title="$WORDPRESS_TITLE" \
+    --admin_email="$WORDPRESS_ADMIN_EMAIL" \
+    --skip-email
+fi
 
 wp theme disable twentyseventeen --network
 wp theme enable twentyseventeen-child --network --activate
+
+echo "<?php
+define('DBI_AWS_ACCESS_KEY_ID', '${DBI_AWS_ACCESS_KEY_ID}');
+define('DBI_AWS_SECRET_ACCESS_KEY', '${DBI_AWS_SECRET_ACCESS_KEY}');
+define('FORCE_SSL_ADMIN', ${FORCE_SSL_ADMIN});
+define('JWT_AUTH_SECRET_KEY', '${JWT_AUTH_SECRET_KEY}');
+define('WP_DEFAULT_THEME', '${WP_DEFAULT_THEME}');
+" > wp-config-headless.php
+
+sed -i "1c<?php require __DIR__ . '/wp-config-headless.php';" wp-config.php
+
+PLUGINS="$( \
+  echo $WORDPRESS_PLUGINS \
+    | sed -e 's/\(\S\+\)\s\S\+\s*/\1\n/g' \
+    | sed -e '/^\s*$/d'
+)"
+
+for plugin_name in $PLUGINS; do
+  wp plugin activate "$plugin_name" --network
+done
+
 EOF
 
 # disable www-data user
