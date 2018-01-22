e54ba3d6bc3c6ebc49a552e0d54e9944f64f88b1
Aleksandr Tsertkov <tsertkov@gmail.com>
2018-01-22 08:18:44 +0100

    Auto-update by wp plugin *

--- wp-headless/twentyseventeen-child/inc/admin-toolbar.php
+++ wp-headless/twentyseventeen-child/inc/admin-toolbar.php
@@ -3,5 +3,4 @@
 add_action( 'admin_bar_menu', function ($wp_adminbar) {
   $wp_adminbar->remove_node('wp-logo');
   $wp_adminbar->remove_node('comments');
-  $wp_adminbar->remove_node('new-content');
-}, 999 );
+}, 9999 );
