125e4225702590bad6c138f9d68e9557e094b6e8
Aleksandr Tsertkov <tsertkov@gmail.com>
2018-12-17 17:14:32 +0100

    Remove unused items from admin menu

--- wordpress/mu-plugins/inc/admin-menu.php
+++ wordpress/mu-plugins/inc/admin-menu.php
@@ -16,9 +16,10 @@ add_action( 'admin_menu', function () {
   remove_submenu_page( 'options-general.php', 'options-permalink.php' );
 } );
 
-add_action( '_network_admin_menu', function () {
+add_action( 'network_admin_menu', function () {
   array_map( 'remove_menu_page', [
     'index.php',
+    'themes.php'
   ] );
 
   remove_submenu_page( 'settings.php', 'setup.php' );
--- wordpress/mu-plugins/inc/admin-toolbar.php
+++ wordpress/mu-plugins/inc/admin-toolbar.php
@@ -3,6 +3,17 @@
 if (!is_admin()) return;
 
 add_action( 'admin_bar_menu', function ($wp_adminbar) {
-  $wp_adminbar->remove_node('wp-logo');
-  $wp_adminbar->remove_node('comments');
+  $wp_adminbar->remove_node( 'wp-logo' );
+  $wp_adminbar->remove_menu( 'network-admin-d' );
+  $wp_adminbar->remove_menu( 'network-admin-t' );
+  $wp_adminbar->remove_menu( 'view-site' );
+  $wp_adminbar->remove_menu( 'edit-site' );
+  $wp_adminbar->remove_menu( 'new-post' );
+  $wp_adminbar->remove_menu( 'new-page' );
+
+  foreach ( get_sites() as $site ) {
+    $wp_adminbar->remove_menu( "blog-{$site->blog_id}-d" );
+    $wp_adminbar->remove_menu( "blog-{$site->blog_id}-n" );
+    $wp_adminbar->remove_menu( "blog-{$site->blog_id}-v" );
+  }
 }, 9999 );
