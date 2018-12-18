387aa21df3f276914518ae0c65c720558c2dfd93
Aleksandr Tsertkov <tsertkov@gmail.com>
2018-12-18 13:42:35 +0100

    Fix disabling right_now and activity dashboard widgets

--- wordpress/mu-plugins/inc/dashboard.php
+++ wordpress/mu-plugins/inc/dashboard.php
@@ -5,7 +5,7 @@ if (!is_admin()) return;
 add_action( 'wp_dashboard_setup', function () {
   remove_meta_box( 'dashboard_primary', 'dashboard', 'side' );      // Wordpress news
   remove_meta_box( 'dashboard_quick_press', 'dashboard', 'side' );  // Quick draft
-  remove_meta_box( 'dashboard_right_now', 'dashboard', 'core' );    // At a glance
-  remove_meta_box( 'dashboard_activity', 'dashboard', 'core' );     // Activity
+  remove_meta_box( 'dashboard_right_now', 'dashboard', 'normal' );  // At a glance
+  remove_meta_box( 'dashboard_activity', 'dashboard', 'normal' );   // Activity
   remove_action( 'welcome_panel', 'wp_welcome_panel' );             // Welcome panel
 } );
