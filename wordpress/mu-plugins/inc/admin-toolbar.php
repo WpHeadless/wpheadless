d5d3357cbe571381c746a932dc6431c22a3ca7d8
Aleksandr Tsertkov <tsertkov@gmail.com>
2019-12-22 21:13:40 +0100

    Change site-name url in toolbar to /wp-admin/

--- wordpress/mu-plugins/inc/admin-toolbar.php
+++ wordpress/mu-plugins/inc/admin-toolbar.php
@@ -10,6 +10,12 @@ add_action( 'admin_bar_menu', function ($wp_adminbar) {
   $wp_adminbar->remove_menu( 'edit-site' );
   $wp_adminbar->remove_menu( 'new-content' );
 
+  // append wp-admin to site-name node href
+  $site_name_node = $wp_adminbar->get_node( 'site-name' );
+  $site_name_node->href = $site_name_node->href . 'wp-admin/';
+  $wp_adminbar->remove_menu( 'site-name' );
+  $wp_adminbar->add_node( $site_name_node );
+
   foreach ( get_sites() as $site ) {
     $wp_adminbar->remove_menu( "blog-{$site->blog_id}-d" );
     $wp_adminbar->remove_menu( "blog-{$site->blog_id}-n" );
