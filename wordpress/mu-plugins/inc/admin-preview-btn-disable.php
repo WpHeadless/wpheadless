82e88367a511cc8b5fe5e46ae3ea7b73fa0eea65
Aleksandr Tsertkov <tsertkov@gmail.com>
2020-01-28 19:23:43 +0100

    Hide admin post preview button

---
+++ wordpress/mu-plugins/inc/admin-preview-btn-disable.php
@@ -0,0 +1,9 @@
+<?php
+
+// hide preview button
+add_action( 'admin_head-post-new.php', 'admin_css_post' );
+add_action( 'admin_head-post.php', 'admin_css_post' );
+
+function admin_css_post () {
+  echo '<style type="text/css">#post-preview, #view-post-btn, #visibility {display: none;}</style>';
+}
