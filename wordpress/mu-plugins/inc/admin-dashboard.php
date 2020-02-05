a7e321670b018f560f473dffd80bc4d12cc41025
Aleksandr Tsertkov <tsertkov@gmail.com>
2020-02-05 21:25:33 +0100

    Redirect network dashboard to network sites

--- wordpress/mu-plugins/inc/admin-dashboard.php
+++ wordpress/mu-plugins/inc/admin-dashboard.php
@@ -4,8 +4,15 @@ if (!is_admin()) return;
 
 // redirect dashboard to "my sites" page
 add_action( 'current_screen', function ( $screen) {
-  if ( 'dashboard' == $screen->id ) {
+  if ( $screen->id === 'dashboard' ) {
     wp_safe_redirect( admin_url( 'my-sites.php' ) );
-    exit;
+
+  } else  if ( $screen->id === 'dashboard-network' ) {
+    wp_safe_redirect( admin_url( 'network/sites.php' ) );
+
+  } else {
+    return;
   }
+
+  exit;
 } );
