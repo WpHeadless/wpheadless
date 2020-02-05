7afddab92da8dbda740258951659521c13a5b800
Aleksandr Tsertkov <tsertkov@gmail.com>
2020-02-05 21:36:05 +0100

    Update footer

--- wordpress/mu-plugins/inc/admin-footer.php
+++ wordpress/mu-plugins/inc/admin-footer.php
@@ -3,5 +3,5 @@
 if (!is_admin()) return;
 
 add_filter( 'admin_footer_text', function () {
-  echo '<span id="footer-thankyou">Thank you for creating with <a href="https://github.com/WpHeadless/wpheadless" target="_blank">WpHeadless</a></span>';
+  echo '<span id="footer-thankyou">Thank you for creating with WordPress <a href="https://github.com/WpHeadless/wpheadless" target="_blank">Headless Bundle</a></span>';
 } );
