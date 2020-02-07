ba6380985940a8064fcc4f0e8b81cdb5a76c63ab
Aleksandr Tsertkov <tsertkov@gmail.com>
2020-02-07 14:02:47 +0100

    Do not allow searching for internal links in wp editor

---
+++ wordpress/mu-plugins/inc/admin-editor-link.php
@@ -0,0 +1,18 @@
+<?php
+
+if (!is_admin()) return;
+
+add_filter( 'wp_link_query', function ( $results ) {
+  foreach( $results as &$result )
+    $result['permalink'] = wp_get_shortlink( $result['ID'] );
+  // return $results;
+  return [];
+} );
+
+add_action( 'admin_head', function () {
+  echo '<style type="text/css">
+  #wp-link #link-selector .howto {display: none;}
+  #wp-link #link-selector #search-panel {display: none;}
+  </style>
+  ';
+} );
